2cf1422e97dec60e14dae2f1279d2b4f31061395
Serdar Sadykov <serdar.sadykov@x5.ru>
2024-10-05 16:27:25 +0300

    fix vite

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type {Preview} from '@storybook/react';
 
-import {ThemeProvider} from 'themeProvider';
+import {ThemeProvider} from '../src/themeProvider';
 
 const preview: Preview = {
   decorators: [
